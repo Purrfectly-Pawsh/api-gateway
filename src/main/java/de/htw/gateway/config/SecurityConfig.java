ac43811ebeb0c37b1717bb187fdc866995c7e88b
Lor0l <o.lordieck@web.de>
2024-06-02 18:24:07 +0200

    WIP try to disable csfr protection, solve problems with ebtaining keys from KC

--- src/main/java/de/htw/gateway/config/SecurityConfig.java
+++ src/main/java/de/htw/gateway/config/SecurityConfig.java
@@ -4,7 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -13,12 +17,18 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // https://docs.spring.io/spring-security/reference/servlet/authorization/authorize-http-requests.html#authorize-requests
+
+        // auf /open kommt immer ein 401 unauthorized status code ... und der gateway server schmeißt ein IllegalStateException: Could not obtain the keys
+
+        // die debug meldungen sagen auch dass das csrf nicht matcht ... vmtl muss das ausgeschaltet werden
+
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/open").permitAll()
                         .requestMatchers("/user").hasRole("USER")
                         .requestMatchers("/admin").hasRole("ADMIN")
-                        .anyRequest().denyAll()
                 );
 
         return http.build();
